632858755221fd9874f70ec8b6e76f14f39eb869
Manuel Roglan <manueljoseph113@gmail.com>
2022-12-16 21:24:49 -0500

    added mui ssr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,28 @@
 import '../styles/globals.css'
+import { ThemeProvider, CssBaseline } from '@mui/material'
+import createCache, { EmotionCache } from '@emotion/cache'
+import { CacheProvider } from '@emotion/react'
+import { theme } from '../styles/theme'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export interface CustomAppProps extends AppProps {
+	emotionCache: EmotionCache;
 }
 
-export default MyApp
+export function createEmotionCache() {
+	return createCache({key: 'css', prepend: true})
+}
+
+const clientSideEmotionCache = createEmotionCache()
+
+export default function MyApp({ Component, pageProps, 
+	emotionCache=clientSideEmotionCache }:CustomAppProps) {
+	return (
+		<CacheProvider value={emotionCache}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</CacheProvider>
+	)
+}
