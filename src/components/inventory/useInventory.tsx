dde7f67b1fe05215b07281f3997b26b484675543
Manuel Roglan <manueljoseph113@gmail.com>
2022-12-22 12:15:08 -0500

    loading initial banks

--- src/components/inventory/useInventory.tsx
+++ src/components/inventory/useInventory.tsx
@@ -2,13 +2,23 @@ import { useCallback, useEffect, useState } from "react";
 import { C_CategoryData } from "../../database/interfaces/Category";
 import { C_PartData } from "../../database/interfaces/Part";
 import axios from 'axios'
+import { C_Ref } from "../../database/interfaces/fauna";
 
 interface CategoryBank {
-    [id:string]: C_CategoryData;
+    [id:string]: {
+        name: string;
+        search: string[];
+        children: string[];
+        parent?: string;
+        parts: string[];
+    };
 }
 
 interface PartBank {
-    [id:string]: C_PartData;
+    [id:string]: {
+        name: string;
+        category: string;
+    };
 }
 
 export default function useInventory(search:string) {
@@ -32,6 +42,35 @@ export default function useInventory(search:string) {
             })
 
             console.log('data', data)
+
+            const catCopy:CategoryBank = {'/': {
+                name: '',
+                search: [],
+                children: [],
+                parts: []
+            }}
+            for (const category of data.categories.data) {
+                catCopy[category.ref['@ref'].id] = {
+                    name: category.data.name,
+                    search: category.data.search,
+                    children: category.data.children.split(','),
+                    parts: category.data.parts.map((p:C_Ref) => p['@ref'].id),
+                    parent: '/'
+                }
+            }
+
+            const pCopy:PartBank = {}
+            for (const part of data.parts) {
+                pCopy[part[0]] = {
+                    name: part[1],
+                    category: '/'
+                }
+            }
+
+            setCategoryBank(catCopy)
+            setPartBank(pCopy)
+            setSearchedCategories(catCopy)
+            setSearchedParts(pCopy)
             setLoading(false)
         } catch (e) {
             console.log(e)
@@ -48,6 +87,9 @@ export default function useInventory(search:string) {
         }
 
     }, [search])
+
+    console.log('categoryBank', categoryBank)
+    console.log('partBank', partBank)
 
     return {
         categories: searchedCategories,
