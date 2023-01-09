d1842347cd9fa5405ba309ef3078cab3fb9dc75f
Manuel Roglan <manueljoseph113@gmail.com>
2023-01-09 16:14:30 -0500

    added location page

---
+++ src/pages/location/index.tsx
@@ -0,0 +1,31 @@
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import MainHeader from "../../components/nav/MainHeader";
+import { getAdminUser } from "../../utils/auth";
+
+export default function Location() {
+
+    return (
+        <>
+            <Head>
+                <title>
+                    Locations | MRG Operations     
+                </title>     
+            </Head> 
+            <div className="root-header-only">
+                <MainHeader loggedIn />
+                <div>main section</div>
+            </div>
+        </>
+    )
+}
+
+export const getServerSideProps:GetServerSideProps = async (ctx) => {
+
+    const {user, redirect} = await getAdminUser(ctx)
+    if (redirect) {
+        return redirect
+    }
+
+    return {props: {user}}
+}
